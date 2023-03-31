e09b06b55185a7aea57c2dd33f98615d77b58a37
Hoa1210 <luonghoa121002@gmail.com>
2023-03-31 22:00:02 +0700

    fix style page chater

--- resources/views/client/pages/comics/chapter.blade.php
+++ resources/views/client/pages/comics/chapter.blade.php
@@ -4,53 +4,71 @@
     <div class="container">
         <div class="title-chapter">
             <h3 class="into-sub-title text-align">{{$detailComic->name}}</h3>
-            <h5 class="into-sub-title text-align">{{$current_chapter->name}}</h5>
+            <h5 class="text-align">{{$current_chapter->name}} - [ Cập nhật lúc: {{date_format($current_chapter->updated_at,'H:i d/m/Y')}}]</h5>
+
         </div>
-        <div class="button-chapter">
+        <div class="button-chapter mb-3">
             @if($previous_chapter == null)
-            <button type="button" class="btn web-btn mt-1">Chương trước</button> 
+            <button type="button" class="btn  mt-1 bg-secondary">
+                <i class="fas fa-chevron-left" aria-hidden="true"></i>
+            </button>
             @else
-            <button type="button" class="btn web-btn mt-1"><a href="{{url('truyen-tranh/'.$detailComic->slug.'/'.$previous_chapter["slug"])}}">Chương trước</a></button> 
+            <button type="button" class="btn web-btn mt-1">
+                <a href="{{url('truyen-tranh/'.$detailComic->slug.'/'.$previous_chapter["slug"])}}">
+                    <i class="fas fa-chevron-left" aria-hidden="true"></i>
+                </a>
+            </button>
             @endif
 
+            <select class="form-select m-1 listchapter">
+                {{-- @dd() --}}
+                <option selected>Chương</option>
+                @foreach($allChapter as $chapter)
+                <option value="{{url('truyen-tranh/'.$detailComic->slug.'/'.$chapter['slug'])}}">{{$chapter['name']}}</option>
+                @endforeach
+            </select>
+
             @if($next_chapter == null)
-            <button type="button" class="btn web-btn mt-1" >Chương sau</button> 
+            <button type="button" class="btn mt-1 bg-secondary">
+                <i class="fas fa-chevron-right" aria-hidden="true"></i>
+            </button>
             @else
-            <button type="button" class="btn web-btn mt-1"><a href="{{url('truyen-tranh/'.$detailComic->slug.'/'.$next_chapter['slug'])}}">Chương sau</a></button> 
+            <button type="button" class="btn web-btn mt-1">
+                <a href="{{url('truyen-tranh/'.$detailComic->slug.'/'.$next_chapter['slug'])}}"><i class="fas fa-chevron-right" aria-hidden="true"></i></a>
+            </button>
             @endif
         </div>
         <div class="list-img">
-            @foreach($img_chapter as $key => $value)    
+            @foreach($img_chapter as $key => $value)
             <div class="box">
                 <img src="{{Storage::url($value)}}" alt="" srcset="">
             </div>
             @endforeach
         </div>
+        <div class="button-chapter-bottom mb-3">
+            @if($previous_chapter == null)
+            <button type="button" class="btn  mt-1 bg-secondary">
+                <i class="fas fa-chevron-left" aria-hidden="true"></i> Chap trước
+            </button>
+            @else
+            <button type="button" class="btn web-btn mt-1">
+                <a href="{{url('truyen-tranh/'.$detailComic->slug.'/'.$previous_chapter["slug"])}}">
+                    <i class="fas fa-chevron-left" aria-hidden="true"></i> Chap trước
+                </a>
+            </button>
+            @endif
+
+            @if($next_chapter == null)
+            <button type="button" class="btn mt-1 bg-secondary">
+                <i class="fas fa-chevron-right" aria-hidden="true"></i> Chap sau
+            </button>
+            @else
+            <button type="button" class="btn web-btn mt-1">
+                <a href="{{url('truyen-tranh/'.$detailComic->slug.'/'.$next_chapter['slug'])}}"><i class="fas fa-chevron-right" aria-hidden="true"></i> Chap sau</a>
+            </button>
+            @endif
+        </div>
     </div>
 </section>
-<style>
-    .list-img{
-        display: flex;
-        flex-direction: column;
-    }
-
-    .box{
-        display: flex;
-        justify-content: center;
-    }
-    .box img {
-        width: 100%;
-        /* max-width: 700px; */
-        position: relative;
-    }
-
-    .button-chapter{
-        display: flex;
-        justify-content: center
-    }
-    .button-chapter button{
-        margin: 5px;
-    }
-</style>
 @endsection
 
