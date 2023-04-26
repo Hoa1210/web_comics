53ec4e9b644e3c86f863977e53650a89ff3c1481
Hoa1210 <luonghoa121002@gmail.com>
2023-04-26 13:51:03 +0700

    create comment table

---
+++ database/migrations/2023_04_26_063921_create_comments_tables.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->unsignedBigInteger('comic_id')->nullable();
+            $table->foreign("comic_id")->references("id")->on("comics");
+            $table->unsignedBigInteger('chapter_id')->nullable();
+            $table->foreign("chapter_id")->references("id")->on("chapters");
+            $table->integer('parent_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
